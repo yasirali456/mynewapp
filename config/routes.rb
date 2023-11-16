bf1a6306edb054d31d25e70294de42f552b24f22
Muhammad Yasir <muhammad.yasir7777@gmail.com>
2023-11-16 23:05:44 +0500

    routes update

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,9 @@ Rails.application.routes.draw do
   resources :articles, only: [:show] 
   devise_for :users
   namespace :me do 
-    resources :articles
-    resources :comments
+    resources :articles do
+      resources :comments
+    end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
